c8effff4ad49189f2e545d6f465be1cd4649222a
WangHotDog <fakeyourgirlfriend@protonmail.com>
2022-04-08 10:21:57 +0800

    Update hw01.go

---
+++ hw01.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+//作业1用 Go 程序打印一首诗,练习输出语句
+func main() {
+	fmt.Println("加\\n方法：")
+	fmt.Println("窗前明月光，疑是地上霜。\n举头望明月，低头思故乡。")
+	fmt.Println("两句话方法：")
+	fmt.Println("窗前明月光，疑是地上霜。")
+	fmt.Println("举头望明月，低头思故乡。")
+	fmt.Print("Print加\\n方法：\n")
+	fmt.Print("窗前明月光，疑是地上霜。\n举头望明月，低头思故乡。")
+}
